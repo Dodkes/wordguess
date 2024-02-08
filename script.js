92c9b6b98f939b41c83aa7aa6bfbb180bf37959e
Jozef_Zitt <dodo.zitt@gmail.com>
2024-02-08 12:18:47 +0100

    game is playable

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 const newWordButton = document.getElementById('newWordButton')
 const wordContainer = document.getElementById('wordContainer')
+const letterButton = document.querySelectorAll('.letter')
+
+let correctWordArray
 
 const words = [
-    'red',
-    'bread',
-    'basement',
-    'electricity',
-    'happiness'
+    'RED',
+    'BREAD',
+    'BASEMENT',
+    'ELECTRICITY',
+    'HAPPINESS'
 ]
 
 newWordButton.addEventListener('click', () => {
     const randomNumber = Math.floor(Math.random() * words.length)
     wordContainer.querySelectorAll('*').forEach(child => child.remove());
 
+    refreshLetters()
+    correctWordArray = words[randomNumber].split('')
     for (i = 0; i < words[randomNumber].length; i++) {
-        console.log(words[randomNumber])
-        console.log(i)
         generateWord()
     }
 })
 
 function generateWord () {
     const letterElement = document.createElement('span')
-    letterElement.textContent = 'A '
+    letterElement.textContent = ' '
     letterElement.classList.add('wordLetter')
     wordContainer.appendChild(letterElement)
+}
+
+letterButton.forEach(button => button.addEventListener('click', () => {
+    if (!correctWordArray) return
+
+    button.classList.add('clicked')
+    button.classList.remove('default')
+    for (i = 0; i < correctWordArray.length; i++) {
+        if (button.textContent === correctWordArray[i]) {
+            wordContainer.childNodes[i].textContent = correctWordArray[i]
+        }
+    }
+
+    const textElements = wordContainer.childNodes
+    const textArray = []
+    textElements.forEach(element => textArray.push(element.textContent))
+
+    if (textArray.toString() === correctWordArray.toString()) {
+        alert('Congrats')
+    }
+}))
+
+function refreshLetters () {
+    console.log('OK')
+        letterButton.forEach(element => {
+            element.classList.add('default')
+            element.classList.remove('clicked')
+    })
 }
